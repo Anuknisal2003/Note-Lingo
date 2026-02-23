7ee5a2e2f12c0af8e308c81af75887d7586ddb22
AnukGamage <140588165+Anuknisal2003@users.noreply.github.com>
2026-02-23 17:03:47 +0530

    Create nl_text_field.dart

---
+++ note_lingo/lib/widgets/nl_text_field.dart
@@ -0,0 +1,54 @@
+// lib/widgets/nl_text_field.dart
+
+import 'package:flutter/material.dart';
+import '../core/theme/app_theme.dart';
+
+class NLTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final IconData icon;
+  final bool obscureText;
+  final Widget? suffix;
+  final TextInputType? keyboardType;
+  final String? Function(String?)? validator;
+  final int? maxLines;
+  final TextInputAction? textInputAction;
+  final VoidCallback? onEditingComplete;
+
+  const NLTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.icon,
+    this.obscureText = false,
+    this.suffix,
+    this.keyboardType,
+    this.validator,
+    this.maxLines = 1,
+    this.textInputAction,
+    this.onEditingComplete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      validator: validator,
+      maxLines: maxLines,
+      textInputAction: textInputAction,
+      onEditingComplete: onEditingComplete,
+      style: Theme.of(context).textTheme.bodyLarge,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14),
+          child: Icon(icon, size: 20, color: AppColors.textMuted),
+        ),
+        prefixIconConstraints: const BoxConstraints(minWidth: 48),
+        suffixIcon: suffix,
+      ),
+    );
+  }
+}
