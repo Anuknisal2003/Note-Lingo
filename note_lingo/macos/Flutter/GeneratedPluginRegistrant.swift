1d0846b92c62916f51bb90f9057316efc3914e16
AnukGamage <140588165+Anuknisal2003@users.noreply.github.com>
2026-02-23 15:53:22 +0530

    Update pubspec.yaml

--- note_lingo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ note_lingo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,24 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import firebase_auth
 import firebase_core
+import firebase_storage
+import google_sign_in_ios
+import open_file_mac
+import record_darwin
+import share_plus
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
