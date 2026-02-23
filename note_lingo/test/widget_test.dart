24deead4b48704a85f9dd9b79e01c1975278e9c9
AnukGamage <140588165+Anuknisal2003@users.noreply.github.com>
2026-02-23 17:10:32 +0530

    Update widget_test.dart

--- note_lingo/test/widget_test.dart
+++ note_lingo/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:note_lingo/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NoteLingo());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
