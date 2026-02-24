81085a92a2611239f8c92634cfc67c10b55b4547
AnukGamage <140588165+Anuknisal2003@users.noreply.github.com>
2026-02-25 01:06:19 +0530

    Update GeneratedPluginRegistrant.swift

--- note_lingo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ note_lingo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,28 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import cloud_firestore
+import connectivity_plus
 import firebase_auth
 import firebase_core
 import firebase_storage
 import google_sign_in_ios
+import just_audio
 import open_file_mac
 import record_darwin
 import share_plus
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
